20acb61915640fe42e0a33d13c83d40b47d1b91f
roomano <romanoivanpedro@gmail.com>
2021-06-24 16:28:36 +0200

    commit inicial

---
+++ src/main/java/com/roomano786/mc/ModelagemconceitualApplication.java
@@ -0,0 +1,13 @@
+package com.roomano786.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemconceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roomano786/mc/ModelagemconceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomano786.mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemconceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
